3d898160e91dab183f97e85776ca55f15b2aed33
jakeycov <127972347+jakeycov@users.noreply.github.com>
2023-03-15 18:54:57 +0000

    Update

--- app.js
+++ app.js
@@ -21,38 +21,9 @@ const messages = [
   }
 ];
 
-const opponentTypes = [
-  {
-    type: "Aggressive Baseline Player",
-    focusPoints: [
-      "Stay patient and consistent during rallies.",
-      "Move your opponent side to side to create openings.",
-      "Mix in drop shots and short angles to draw them forward.",
-    ],
-  },
-  {
-    type: "Serve and Volley Player",
-    focusPoints: [
-      "Aim for deep, well-placed returns to limit their volley options.",
-      "Mix in passing shots and lobs to keep them guessing.",
-      "Stay aggressive on your own serve to maintain pressure.",
-    ],
-  },
-  {
-    type: "Defensive Counterpuncher",
-    focusPoints: [
-      "Stay patient and wait for the right opportunity to attack.",
-      "Use varied pace and spin to disrupt their rhythm.",
-      "Stay aggressive and take the initiative when possible.",
-    ],
-  },
-  // Add more opponent types and focus points here
-];
-
 const headlineEl = document.querySelector(".headline");
 const sentenceEl = document.querySelector(".sentence");
 const btnEl = document.querySelector(".btn");
-const opponentSelect = document.getElementById("opponent");
 
 function displayMessage() {
   const randomIndex = Math.floor(Math.random() * messages.length);
@@ -62,24 +33,7 @@ function displayMessage() {
   sentenceEl.textContent = message.sentence;
 }
 
-function displayOpponentFocus() {
-  const selectedIndex = opponentSelect.selectedIndex;
-  const opponent = opponentTypes[selectedIndex];
-  const focusList = document.getElementById("focus-list");
-
-  focusList.innerHTML = "";
-  opponent.focusPoints.forEach((point) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = point;
-    focusList.appendChild(listItem);
-  });
-}
-
 btnEl.addEventListener("click", displayMessage);
 
-// Add event listener and call it initially to set the focus points
-opponentSelect.addEventListener("change", displayOpponentFocus);
-opponentSelect.dispatchEvent(new Event("change"));
-
-// Display initial message
+// Display an initial message
 displayMessage();
